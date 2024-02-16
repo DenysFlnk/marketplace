deb9c93c76cb85e0113c8860455980f13dc7d18a
Denys Filonenko <filonenko.denys94@gmail.com>
2024-02-16 09:13:47 +0200

    initial commit

---
+++ src/main/resources/db/init.sql
@@ -0,0 +1,98 @@
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS user_favorites;
+DROP TABLE IF EXISTS product_rating;
+DROP TABLE IF EXISTS product_img;
+DROP TABLE IF EXISTS product_category;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS user_img;
+DROP TABLE IF EXISTS user_role;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS users;
+
+
+CREATE TABLE users (
+	id 			SERIAL PRIMARY KEY,
+	username 	VARCHAR,
+	email 		VARCHAR,
+	us_password VARCHAR
+);
+
+CREATE TABLE roles (
+	id 			SERIAL PRIMARY KEY,
+	name		VARCHAR
+);
+
+CREATE TABLE user_role (
+	user_id 	INTEGER NOT NULL,
+	role_id 	INTEGER NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+	FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE,
+	CONSTRAINT user_role_idx UNIQUE (user_id, role_id)
+);
+
+CREATE TABLE user_img (
+	user_id 	INTEGER NOT NULL,
+	img_url		VARCHAR,
+	FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+	CONSTRAINT user_idx UNIQUE (user_id)
+);
+
+CREATE TABLE product (
+	id 			SERIAL PRIMARY KEY,
+	creator_id  INTEGER NOT NULL,
+	quantity 	INTEGER NOT NULL,
+	price 		INTEGER NOT NULL,
+	description TEXT,
+	FOREIGN KEY (creator_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE category (
+	id 			SERIAL PRIMARY KEY,
+	name		VARCHAR
+);
+
+CREATE TABLE product_category (
+	product_id 	INTEGER NOT NULL,
+	category_id	INTEGER NOT NULL,
+	FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE,
+	FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE
+);
+
+CREATE TABLE product_img (
+	id 			SERIAL PRIMARY KEY,
+	product_id 	INTEGER NOT NULL,
+	img_url		VARCHAR,
+	FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_favorites (
+	user_id 	INTEGER NOT NULL,
+	product_id 	INTEGER NOT NULL,
+	FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE,
+	FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE product_rating (
+	id 			SERIAL PRIMARY KEY,
+	product_id 	INTEGER NOT NULL,
+	user_id 	INTEGER NOT NULL,
+	rating		INTEGER NOT NULL,
+	comment     TEXT,
+	FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE,
+	FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE orders (
+	id 			SERIAL PRIMARY KEY,
+	product_id 	INTEGER NOT NULL,
+	user_id 	INTEGER NOT NULL,
+	quantity 	INTEGER NOT NULL,
+	total_price INTEGER NOT NULL,
+	status		VARCHAR,
+	FOREIGN KEY (product_id) REFERENCES product (id),
+	FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+
+
---
+++ src/main/resources/db/populate.sql
@@ -0,0 +1,103 @@
+INSERT INTO users (id, username, email, us_password)
+VALUES 	(1, 'John', 'john@gmail.com', 'johnsPassword'),
+		(2, 'Suarez', 'suarez@gmail.com', 'suarezPassword'),
+		(3, 'Julia', 'julia@gmail.com', 'juliasPassword'),
+		(4, 'Funny Shop', 'funny.shop@gmail.com', 'funny1shopsPassword'),
+		(5, 'Tiffany`s Jewelry', 'tiffanys.jewelry@gmail.com', 'tiffanys1jewelryPassword'),
+		(6, 'Embroidery Shop', 'embroidery.shop@gmail.com', 'embroidery1shopsPassword'),
+		(7, 'Leather World', 'leather.world@gmail.com', 'leather1worldPassword');
+
+INSERT INTO user_img (user_id, img_url)
+VALUES  (1, 'placeholder'),
+		(2, 'placeholder'),
+		(3, 'placeholder'),
+		(4, 'placeholder'),
+		(5, 'placeholder'),
+		(6, 'placeholder'),
+		(7, 'placeholder');
+
+INSERT INTO roles (id, name)
+VALUES  (1, 'USER'),
+		(2, 'ADMIN');
+
+INSERT INTO user_role (user_id, role_id)
+VALUES  (1, 2),
+		(2, 1),
+		(3, 1),
+		(4, 1),
+		(5, 1),
+		(6, 1),
+		(7, 1);
+
+INSERT INTO category (id, name)
+VALUES  (1, 'Ceramic'),
+		(2, 'Wood'),
+		(3, 'Metal'),
+		(4, 'Woolen'),
+		(5, 'Embroidery'),
+		(6, 'Leather'),
+		(7, 'Jewelry'),
+		(8, 'Present'),
+		(9, 'Glass');
+		
+INSERT INTO product (id, creator_id, quantity, price, description)
+VALUES  (1, 4, 10, 600, 'Смішна футболка'),
+    	(2, 4, 5, 280, 'Смішна кружка'),
+    	(3, 5, 20, 1500, 'Діамантове кольє'),
+    	(4, 5, 15, 1200, 'Золотий браслет'),
+    	(5, 6, 8, 1000, 'Вишивана подушка'),
+    	(6, 6, 12, 550, 'Вишиваний рушник'),
+    	(7, 7, 25, 2800, 'Шкіряна куртка'),
+    	(8, 7, 18, 1900, 'Шкіряна сумка'),
+    	(9, 4, 30, 150, 'Смішна шапка'),
+    	(10, 6, 10, 800, 'Вишивана сукня');
+
+INSERT INTO product_img (id, product_id, img_url)
+VALUES  (1, 1, 'placeholder1'),
+		(2, 1, 'placeholder2'),
+    	(3, 2, 'placeholder1'),
+		(4, 2, 'placeholder2'),
+    	(5, 3, 'placeholder1'),
+		(6, 4, 'placeholder2'),
+    	(7, 5, 'placeholder1'),
+    	(8, 6, 'placeholder1'),
+    	(9, 6, 'placeholder2'),
+		(10, 6, 'placeholder3'),
+		(11, 6, 'placeholder4'),
+    	(12, 7, 'placeholder1'),
+    	(13, 8, 'placeholder1'),
+    	(14, 9, 'placeholder1'),
+    	(15, 10, 'placeholder1');
+		
+INSERT INTO product_category (product_id, category_id)
+VALUES	(1, 8),
+    	(2, 1),
+		(2, 8),
+    	(3, 7),
+		(3, 8),
+    	(4, 7),
+		(4, 8),
+    	(5, 5),
+    	(6, 5),
+    	(7, 6),
+    	(8, 6),
+    	(9, 8),
+    	(10, 5);
+
+INSERT INTO user_favorites (user_id, product_id)
+VALUES  (3, 2);
+
+INSERT INTO product_rating  (id, product_id, user_id, rating, comment)
+VALUES  (1, 4, 3, 5, 'Дуже гарний браслет!');
+
+INSERT INTO orders  (id, product_id, user_id, quantity, total_price, status)
+VALUES  (1, 4, 3, 1, 1200, 'COMPLETED'),
+		(2, 6, 2, 2, 1100, 'IN_PROGRESS'),
+		(3, 9, 1, 1, 150, 'CANCELED');
+
+
+
+
+
+
+
